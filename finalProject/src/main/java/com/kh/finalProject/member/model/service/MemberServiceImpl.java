bd91748339dd9738804587d080ea0bbe018a99ce
Choechang <dame9735@naver.com>
2023-12-04 12:09:30 +0900

    12-09 12:09

--- finalProject/src/main/java/com/kh/finalProject/member/model/service/MemberServiceImpl.java
+++ finalProject/src/main/java/com/kh/finalProject/member/model/service/MemberServiceImpl.java
@@ -1,8 +1,23 @@
 package com.kh.finalProject.member.model.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kh.finalProject.member.model.dao.MemberDao;
+import com.kh.finalProject.member.model.vo.Member;
 
 @Service
 public class MemberServiceImpl implements MemberService {
-
+	@Autowired
+	private MemberDao memberDao;
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Override
+	public int insertMember(Member m) {
+		
+		return memberDao.insertMember(sqlSession, m);
+	}
 }
--- finalProject/src/main/java/com/kh/finalProject/member/model/vo/Member.java
+++ finalProject/src/main/java/com/kh/finalProject/member/model/vo/Member.java
@@ -1,5 +1,36 @@
 package com.kh.finalProject.member.model.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * 
+ * setter , getter 롬북 사용
+ *
+ */
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Member {
 
+	private int memberNo;
+	private String memberEmail;
+	private String memberPwd;
+	private String memberName;
+	private String memberConcern;
+	private int memberPro;
+	private String phone;
+	private String location;
+	private String status;
+	private String memberGender;
+	private int point;
+	private String filePath;
+	private int account;
+	
 }
