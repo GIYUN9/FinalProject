172c68d1247fe94118bbd4356aa6866ef3a84403
Choechang <dame9735@naver.com>
2023-12-04 12:43:49 +0900

    12-0 12.43

--- finalProject/src/main/java/com/kh/finalProject/member/model/dao/MemberDao.java
+++ finalProject/src/main/java/com/kh/finalProject/member/model/dao/MemberDao.java
@@ -1,8 +1,13 @@
 package com.kh.finalProject.member.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MemberDao {
+	
+	public int insertMember(SqlSessionTemplate sqlSession, Member m) {
+		return sqlSession.insert("memberMapper.insertMember", m);
+	}
 
 }
